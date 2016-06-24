7c1a00d51ca090b8aeb19b342af5c8731dca3ff3
9v1d0 <gviioo2@gmail.com>
2016-06-24 13:31:12 +0300

    initial commit

---
+++ api/controllers/GAPIController.js
@@ -0,0 +1,11 @@
+/**
+ * GAPIController
+ *
+ * @description :: Server-side logic for managing gapis
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
